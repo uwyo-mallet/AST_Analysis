8510ea5ba6a1ce917a27f15994883f4db4ccc085
Grace Abawe <91027100+graceabawe@users.noreply.github.com>
2024-07-26 16:15:12 -0600

    Update to allow for multiple file inputs
    Also saves output as .csv format

---
+++ README.txt
@@ -0,0 +1,39 @@
+July 2024
+
+University of Wyoming, College of Engineering
+Lars Kotthoff
+Grace Abawe
+
+Python scripts for extracting graph characteristics from Abstract Syntax Trees
+
+For C code:
+First, generate the AST using Clang by running the following command in your terminal:
+
+    clang -Xclang -ast-dump=json -fsyntax-only <sourcefile.c> > ast.txt
+
+
+Ensure that the imported modules are installed and that the python virtual environment is activated.
+Then, execute the python script to parse and extract characteristics from the AST and visualize it by running the following:
+
+    python3 ast_analysis.py
+
+--------------------------------------------------------------------------------------------------------------------
+
+For python code:
+To build the ASTs, extract characteristics, and visualize, run the following:
+
+    python3 python_ast_analysis.py <sourcefiles.py> 
+
+Multiple file paths can be specificied, it will prompt you for wether you would like to visualize graphs, and the output will be saved in 'results.csv'.
+
+---------------------------------------------------------------------------------------------------------------------
+
+Example code files have been included in the ast_analysis folder, such as simple.c, matrix.c, and simple.py
+
+Common issues:
+If experiencing issues with uninstalled modules, try running:
+    pip install networkx matplotlib
+    sudo apt-get install graphviz
+
+Tutorial for creating and activating a virtual environment:
+    https://python.land/virtual-environments/virtualenv
--- python_ast_analysis.py
+++ python_ast_analysis.py
@@ -1,7 +1,11 @@
+# Uses Python Abstract Syntax Trees to extract graph characteristics
+
 import ast
 import networkx as nx
 from networkx.drawing.nx_pydot import graphviz_layout
 import matplotlib.pyplot as plt
+import csv
+import os
 
 #Parse Python AST and build a graph
 class BuildAST(ast.NodeVisitor):
@@ -31,17 +35,14 @@ class BuildAST(ast.NodeVisitor):
 
 # Function to extract graph characteristics
 def analyze_graph(G):
-    num_nodes = G.number_of_nodes()
-    num_edges = G.number_of_edges()
-    degrees = dict(G.degree())
-    transitivity = nx.transitivity(G)
     depth = nx.dag_longest_path_length(G) if nx.is_directed_acyclic_graph(G) else None
-    
-    print("Number of nodes:", num_nodes)
-    print("Number of edges:", num_edges)
-    print("Degrees of the nodes:", degrees)
-    print("Transitivity:", transitivity)
-    print("Depth of the tree:", depth)
+    return {
+        "Nodes": G.number_of_nodes(),
+        "Edges":  G.number_of_edges(),
+        "Degrees": dict(G.degree()),
+        "Transitivity":  nx.transitivity(G),
+        "Depth": depth 
+    }
 
 # Function to visualize the graph
 def visualize_graph(G):
@@ -51,19 +52,52 @@ def visualize_graph(G):
             font_size=8, font_weight="bold", arrows=True)
     plt.show()
 
-def main(file_path):
-    with open(file_path, 'r') as file:
+#Function to create graph out of AST
+def process_file(path, bool):
+    with open(path, 'r') as file:
         python_code = file.read()
     root = ast.parse(python_code)
     build = BuildAST()
     G = build.build_graph(root)
-    analyze_graph(G)
-    visualize_graph(G)
+    stats = analyze_graph(G)
+    if (bool.casefold() == 'yes'): #visualize graph
+        visualize_graph(G)
+    return stats
+
+#Function to print aggregate statistics
+def aggregate_stats(results):
+    print("Aggregate Statistics:")
+    print("Total Nodes:", sum(result['Nodes'] for result in results))
+    print("Total Edges:", sum(result['Edges'] for result in results))
+    print("Average Transitivity:", sum(result['Transitivity'] for result in results) / len(results))
+    print("Max Depth:", max(result['Depth'] for result in results))
+
+#Function to output to .csv file
+def write_csv(results, output):
+    with open(output, 'w', newline='') as file:
+        columns = ['File', 'Nodes', 'Edges', 'Degrees', 'Transitivity', 'Depth']
+        writer = csv.DictWriter(file, fieldnames=columns)
+        writer.writeheader()
+        for result in results:
+            writer.writerow(result)
+
+
+def main(file_paths, output, bool):
+    results = []
+    for file_path in file_paths:
+        stats = process_file(file_path, bool)
+        stats['File'] = file_path
+        results.append(stats)
+    write_csv(results, output)
+    aggregate_stats(results)
+        
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 2:
-        file_path = sys.argv[1]
-        main(file_path)
+    if len(sys.argv) >= 2:
+        file_paths = sys.argv[1:] #take multiple files
+        output = 'results.csv'
+        boolean = input('Would you like to visualize the inputs as graphs? (yes/no)')
+        main(file_paths, output, boolean)
     else:
-        print("Error: specify python file path as second argument")
+        print("Error: Specify python file path(s) as second argument")
