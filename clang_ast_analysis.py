923e1ebc6d32c3f912a79267592c4162bcbe905f
Grace Abawe <91027100+graceabawe@users.noreply.github.com>
2024-08-24 19:15:15 -0600

    Output to terminal and add -g flag

--- clang_ast_analysis.py
+++ clang_ast_analysis.py
@@ -5,8 +5,8 @@ from networkx.drawing.nx_pydot import graphviz_layout
 import matplotlib.pyplot as plt
 from scipy.stats import entropy
 import numpy as np
-import csv
 import re
+import argparse
 
 def generate_ast(c_file_path, ast_file_path):
     clang_command = f"clang -Xclang -ast-dump=json -fsyntax-only {c_file_path} > {ast_file_path}"
@@ -57,7 +57,7 @@ def analyze_graph(G):
         avg_shortest_path = nx.average_shortest_path_length(subgraph)
         avg_eccentricity = np.mean(list(nx.eccentricity(subgraph).values()))
     edge_density = G.number_of_edges() / (G.number_of_nodes() * (G.number_of_nodes() - 1)) if G.number_of_nodes() > 1 else 0
-    
+
     node_types = set()
     edge_transitions = []
 
@@ -98,7 +98,6 @@ def analyze_graph(G):
         "pagerank": nx.pagerank(G, max_iter=500),
         "edge_density": edge_density,
         "average_shortest_path": avg_shortest_path,
-        #Node type and edge type transitions
         "node_types": sorted(list(node_types)),
         "edge_transitions": edge_transitions
     }
@@ -121,40 +120,62 @@ def aggregate_stats(results):
     print("Average Eccentricity:", np.mean([result['average_eccentricity'] for result in results]))
     print("Average Edge Density:", np.mean([result['edge_density'] for result in results]))
 
-def write_csv(results, output):
-    with open(output, 'w', newline='') as csv_file:
-        columns = ['file', 'num_nodes', 'num_edges', 'degrees', 'max_degree', 'min_degree', 'mean_degree', 
-                'degree_variance', 'transitivity', 'depths', 'max_depth', 'min_depth', 'mean_depth',
-                'clustering_coefficients', 'max_clustering', 'min_clustering', 'mean_clustering', 'clustering_variance',
-                'degree_entropy', 'depth_entropy', 'betweenness_centrality', 'eigenvector_centrality',
-                'assortativity', 'average_eccentricity', 'diameter', 'radius', 'pagerank', 'edge_density',
-                'average_shortest_path', 'node_types', 'edge_transitions']
-        writer = csv.DictWriter(csv_file, fieldnames=columns)
-        writer.writeheader()
-        for result in results:
-            writer.writerow(result)
+def print_results(stats, file):
+    print("Statistics for file:", file)
+    print("Number of nodes:", stats['num_nodes'])
+    print("Number of Edges:", stats['num_edges'])
+    print("Degrees:", stats['degrees'])
+    print("Maximum Degree:", stats['max_degree'])
+    print("Minimum Degree:", stats['min_degree'])
+    print("Mean Degree:", stats['mean_degree'])
+    print("Degree Variance:", stats['degree_variance'])
+    print("Transitivity:", stats['transitivity'])
+    print("Leaf Depths:", stats['depths'])
+    print("Max Depth:", stats['max_depth'])
+    print("Min Depth:", stats['min_depth'])
+    print("Mean Depth:", stats['mean_depth'])
+    print("Clustering Coefficients:", stats['clustering_coefficients'])
+    print("Max Clustering:", stats['max_clustering'])
+    print("Min Clustering:", stats['min_clustering'])
+    print("Mean Clustering:", stats['mean_clustering'])
+    print("Clustering Variance:", stats['clustering_variance'])
+    print("Degree Entropy:", stats['degree_entropy'])
+    print("Depth Entropy:", stats['depth_entropy'])
+    print("Betweenness Centrality:", stats['betweenness_centrality'])
+    print("Eigenvector Centrality:", stats['eigenvector_centrality'])
+    print("Assortativity:", stats['assortativity'])
+    print("Average Eccentricity:", stats['average_eccentricity'])
+    print("Diameter:", stats['diameter'])
+    print("Radius:", stats['radius'])
+    print("Pagerank:", stats['pagerank'])
+    print("Edge Density:", stats['edge_density'])
+    print("Average Shortest Path:", stats['average_shortest_path'])
+    print("Node Types:", stats['node_types'])
+    print("Edge Type Transitions:", stats['edge_transitions'])
+    print("")
 
-def main(file_paths, output, bool):
+def main(file_paths, visualize):
     results = []
     for file_path in file_paths:
         generate_ast(file_path, "ast_output.txt")
         ast = load_ast("ast_output.txt")
         G = parse_ast(ast)
         stats = analyze_graph(G)
-        if (bool.casefold() == 'yes'): #visualize graph
+        if (visualize==True): #visualize graph
             visualize_graph(G)
-        stats['file'] = file_path
         results.append(stats)
-    write_csv(results, output)
+        print_results(stats, file_path)
     aggregate_stats(results)
-
 
 if __name__ == "__main__":
     import sys
     if len(sys.argv) >= 2:
         file_paths = sys.argv[1:] #take multiple files
-        output = 'c_results.csv'
-        boolean = input('Would you like to visualize the inputs as graphs? (yes/no)')
-        main(file_paths, output, boolean)
+        parser = argparse.ArgumentParser(description="Analyze Clang ASTs and extract graph features.")
+        #The -g flag should be specific before input C files to visualize graphs
+        parser.add_argument("-g", "--graph", action="store_true", help="Visualize the graph after processing.")
+        parser.add_argument("files", nargs='+', help="List of input Clang AST files to process.")
+        args = parser.parse_args()
+        main(file_paths=args.files, visualize=args.graph)
     else:
-        print("Error: Specify C file path(s) as second argument")    
+        print("Error: Specify C file path(s).")    
